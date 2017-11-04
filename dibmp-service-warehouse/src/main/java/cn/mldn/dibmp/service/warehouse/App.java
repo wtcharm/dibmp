ad0b668fb40f25040af4b54015b680599c050a46
fyh <1025173312@qq.com>
2017-11-05 02:25:31 +0800

    分销管理系统：基础环境开发

---
+++ dibmp-service-warehouse/src/main/java/cn/mldn/dibmp/service/warehouse/App.java
@@ -0,0 +1,13 @@
+package cn.mldn.dibmp.service.warehouse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
